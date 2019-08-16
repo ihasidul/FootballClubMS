f493b81de05f02f805c7e78f750c23af414a707b
Hasidul Islam <ihasidul@gmail.com>
2019-08-16 15:09:56 +0600

    Entity files are added. Not done with the files yet.

---
+++ Entity/EmployeeEntity.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using FootballClubMS.Applications;
+using FootballClubMS.Data;
+using System.Windows.Forms;
+
+namespace FootballClubMS.Entity
+{
+    public class EmployeeEntity
+    {
+        private string id;
+        private string name;
+        private string designation;
+        private string email;
+        private double  salary;
+
+        public string Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public string Designation
+        {
+            get { return this.designation; }
+            set { this.designation = value; }
+        }
+        public string Email
+        {
+            get { return this.email; }
+            set { this.email = value; }
+        }
+        public double Salary
+        {
+            get { return this.salary; }
+            set { this.salary = value; }
+        }
+
+        public EmployeeEntity(string name, string designation, string email, double salary)
+        {
+            this.Name = name;
+            this.Designation = designation;
+            if (Designation.ToLower().Equals("admin"))
+            {
+                int tempid = UserRepository.GetTotalUsers() + 1;
+                this.ID = "a-" + tempid.ToString().PadLeft(6, '0') + "-1";
+            }
+            else if (Designation.ToLower().Equals("manager"))
+            {
+                int tempid = UserRepository.GetTotalUsers() + 1;
+                this.ID = "m-" + tempid.ToString().PadLeft(6, '0') + "-2";
+            }
+            
+            else if (Designation.ToLower().Equals("seller"))
+            {
+                int tempid = UserRepository.GetTotalUsers() + 1;
+                this.ID = "s-" + tempid.ToString().PadLeft(6, '0') + "-4";
+            }
+            this.Email = email;
+            
+        }
+    }
+}
---
+++ Entity/LoginEntity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using FootballClubMS.Applications;
+using FootballClubMS.Data;
+using System.Windows.Forms;
+namespace FootballClubMS.Entity
+{
+    public class LoginEntity
+    {
+        private string id;
+        private string password;
+
+        public  string Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public string Password
+        {
+            get { return this.password; }
+            set { this.password = value; }
+        }
+        public LoginEntity(DataSet data)
+        {
+            try
+            {
+                this.Id = data.Tables[0].Rows[0][0].ToString();
+                this.Password = data.Tables[0].Rows[0][1].ToString();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Something is wrong.");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
